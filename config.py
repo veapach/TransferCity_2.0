70fd74521d2928924d531ca5df61ea1d16e24619
veapach <egor_chapaev@bk.ru>
2024-10-02 18:37:52 +0300

    TG_TOKEN in config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
+#
+# load_dotenv()
 
-load_dotenv()
-
-TG_TOKEN = os.getenv('TG_TOKEN')
+#TG_TOKEN = os.getenv('TG_TOKEN')
+TG_TOKEN = '7629905619:AAFKuEY1t0N3zSvJFps3aC1rLLpFVd3L89s'
